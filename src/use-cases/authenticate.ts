2c4a1bb54bc3976637d55eb2ee641f407373f9c3
Daniel Sângelo <danielsangelo@hotmail.com>
2024-08-28 20:45:52 -0300

    Create authenticate use case

---
+++ src/use-cases/authenticate.ts
@@ -0,0 +1,29 @@
+import { UsersRepository } from '@/repositories/users-repository'
+import { User } from '@prisma/client'
+import { compare } from 'bcryptjs'
+import { InvalidCredentialsException } from './errors/invalid-credentials-exception'
+
+interface AuthenticateUseCaseRequestDTO {
+  email: string
+  password: string
+}
+
+interface AuthenticateUseCaseResponseDTO {
+  user: User
+}
+
+export class AuthenticateUseCase {
+  constructor(private usersRepository: UsersRepository) {}
+  async execute({
+    email,
+    password,
+  }: AuthenticateUseCaseRequestDTO): Promise<AuthenticateUseCaseResponseDTO> {
+    const user = await this.usersRepository.findByEmail(email)
+    if (!user) throw new InvalidCredentialsException()
+    const doesPasswordMatch = await compare(password, user.password)
+    if (!doesPasswordMatch) throw new InvalidCredentialsException()
+    return {
+      user,
+    }
+  }
+}
