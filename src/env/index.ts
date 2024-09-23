d5fe67dc4a5aeba63f88dbf31315aa27b8bf02f2
Daniel Sângelo <danielsangelo@hotmail.com>
2024-09-23 19:17:32 -0300

    JWT auth

--- src/env/index.ts
+++ src/env/index.ts
@@ -2,15 +2,16 @@ import 'dotenv/config'
 import { z } from 'zod'
 
 const envSchema = z.object({
-    NODE_ENV: z.enum(['dev', 'production', 'test']).default('dev'),
-    PORT: z.coerce.number().default(3333)
+  NODE_ENV: z.enum(['dev', 'production', 'test']).default('dev'),
+  PORT: z.coerce.number().default(3333),
+  JWT_SECRET: z.string(),
 })
 
 const _env = envSchema.safeParse(process.env)
 
 if (!_env.success) {
-    console.error('Invalid environment variales', _env.error.format())
-    throw new Error('Invalid environment variales')
+  console.error('Invalid environment variales', _env.error.format())
+  throw new Error('Invalid environment variales')
 }
 
 export const env = _env.data
--- src/http/controllers/authenticate.ts
+++ src/http/controllers/authenticate.ts
@@ -1,5 +1,3 @@
-import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
-import { AuthenticateUseCase } from '@/use-cases/authenticate'
 import { InvalidCredentialsException } from '@/use-cases/exceptions/invalid-credentials-exception'
 import { makeAuthenticateUseCase } from '@/use-cases/factories/make-authenticate-use-case'
 import { FastifyReply, FastifyRequest } from 'fastify'
@@ -17,14 +15,22 @@ export async function authenticate(
   const { email, password } = authenticateBodySchema.parse(request.body)
   try {
     const authenticateUseCase = makeAuthenticateUseCase()
-    await authenticateUseCase.execute({
+    const { user } = await authenticateUseCase.execute({
       email,
       password,
     })
+    const token = await reply.jwtSign(
+      {},
+      {
+        sign: {
+          sub: user.id,
+        },
+      },
+    )
+    return reply.status(200).send({ access_token: token })
   } catch (error) {
     if (error instanceof InvalidCredentialsException) {
       return reply.status(400).send({ message: error.message })
     }
   }
-  return reply.status(200).send()
 }
